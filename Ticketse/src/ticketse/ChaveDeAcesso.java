538be66469361e91b7e207156c85f1ca9d9f0ca0
glooooh <56931444+glooooh@users.noreply.github.com>
2019-12-08 23:44:12 -0300

    Ajuste do método main

--- Ticketse/src/ticketse/ChaveDeAcesso.java
+++ Ticketse/src/ticketse/ChaveDeAcesso.java
@@ -110,7 +110,7 @@ public class ChaveDeAcesso extends javax.swing.JDialog {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public void run() {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
